0ac8b017cf8e1080d36e5e1d3e83cf63d208988c
NATTAPONG\Jonus.co.th <Nattapong_Jonus@outlook.com>
2025-02-25 16:23:14 +0700

    Update build and manifest files for improved structure and performance

--- raddaran-web-ai-challenger/.next/server/server-reference-manifest.js
+++ raddaran-web-ai-challenger/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8GsSPRLZbZNbO7n7Xy59jkUiN7Qx/brJFhtm5GF6gbY=\"\n}"
